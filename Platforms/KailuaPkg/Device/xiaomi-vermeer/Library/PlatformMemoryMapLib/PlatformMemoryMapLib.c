0eb072fd46cd29912a399d6945716bf195d1fff0
Mortal <mortal1063@qq.com>
2026-05-26 21:31:37 +0800

    Update PlatformMemoryMapLib.c

--- Platforms/KailuaPkg/Device/xiaomi-vermeer/Library/PlatformMemoryMapLib/PlatformMemoryMapLib.c
+++ Platforms/KailuaPkg/Device/xiaomi-vermeer/Library/PlatformMemoryMapLib/PlatformMemoryMapLib.c
@@ -60,17 +60,6 @@ static ARM_MEMORY_REGION_DESCRIPTOR_EX gDeviceMemoryDescriptorEx[] = {
 //    { "RAM Partition",     0x8C0000000, 0x80000000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK_XN},
 //    { "RAM Partition",     0x940000000, 0x40000000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK_XN},
 
-#if USE_MEMORY_FOR_SERIAL_OUTPUT == 1
-    { "PStore",            0x940000000, 0x00400000, AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_THROUGH_XN},
-    { "RAM Partition",     0x940400000, 0x3FC00000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK_XN},
-#else
-    { "RAM Partition",     0x940000000, 0x40000000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK_XN},
-#endif
-
-    { "RAM Partition",     0x980000000, 0x80000000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK_XN},
-    { "RAM Partition",     0xA00000000, 0x80000000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK_XN},
-    { "RAM Partition",     0xA80000000, 0x80000000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK_XN},
-
 
     /* Other memory regions */
     {"AOP_SS_MSG_RAM",    0x0C300000, 0x00100000, NoHob,  MMAP_IO, INITIALIZED, Conv,   NS_DEVICE},
